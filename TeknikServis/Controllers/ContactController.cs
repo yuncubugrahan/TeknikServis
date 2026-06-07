4acbcdc3d84962d1fb0f6d3ad3eab93f9223cf52
yuncubugrahan <yuncubugrahan@gmail.com>
2026-06-07 11:42:43 +0300

    İletişim

---
+++ TeknikServis/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeknikServis.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
